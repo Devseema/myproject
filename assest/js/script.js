68ed95d953513922a91444f3fa4b89d39fb91253
Devseema <devseema6305@gmail.com>
2024-07-27 12:19:45 +0530

    my first commit

---
+++ assest/js/script.js
@@ -0,0 +1,36 @@
+const slides=document.querySelectorAll('.slider');
+var counter=0;
+slides.forEach(
+    (slide,index)=>{
+        // console.log(slide,index);
+        slide.style.left = `${index*100}%`
+    }
+)
+// ``=backtics
+
+const goNext = () =>{
+ counter++
+ //0,1,2,3
+ if(counter==slides.length){
+    counter=0;
+ }
+ slideImage()
+}
+const goPrev = () =>{
+    // 4,3,2,1,0
+    if(counter<0){
+       counter= slides.length-1;
+    }
+ counter--
+ slideImage()
+}
+
+const slideImage = () => {
+    slides.forEach(
+        (slide)=>{
+            console.log(slide,counter);
+            // slide.style.transform = 'transalteX(-${counter * 100}%)'
+            slide.style.transform = `translateX(-${counter * 100}%)`;
+        }
+    )
+}
